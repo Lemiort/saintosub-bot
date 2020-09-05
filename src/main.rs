eca50f04456d55de8975f632b9ff8ec3cea31b75
Lemiort <lemiort@gmail.com>
2020-09-05 21:27:15 +0300

    Temp

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,15 @@
-extern crate rawr;
+mod memes;
+
 use std::{collections::HashSet, env, time};
 
 use tokio::sync::watch;
 use tokio::sync::watch::Receiver;
 use tokio::sync::watch::Sender;
 
-use futures::executor::block_on;
-use futures::StreamExt;
-use rand::seq::SliceRandom;
-use telegram_bot::prelude::*;
-use telegram_bot::{
-    Api, Error, GetMe, InputFileRef, Message, MessageChat, MessageKind, MessageOrChannelPost,
-    UpdateKind, User,
+use teloxide::{
+    prelude::*,
+    types::{Chat, InputFile, MediaKind, MessageKind, User},
 };
-
-use rawr::prelude::*;
 
 // const FOOL_PIC_LINK: &str = "https://c7.hotpng.com/preview/259/820/839/dio-brando-jojo-s-bizarre-adventure-eyes-of-heaven-youtube-diamond-is-unbreakable-just-cause-thumbnail.jpg";
 // const APPROACHING_PIC_LINK: &str = "https://i.ytimg.com/vi/IJJM_ccGxSQ/maxresdefault.jpg";
@@ -23,89 +18,71 @@ const ROAD_ROLLER_PIC_LINK: &str = "https://i.ytimg.com/vi/t1y3QOIRsYs/maxresdef
 const DOESNT_MATTER_PIC_LINK: &str =
     "https://i.kym-cdn.com/entries/icons/original/000/029/407/Screenshot_14.jpg";
 
-const PIGS_LINKS: &'static [&'static str] = &[
-    "https://cs10.pikabu.ru/post_img/2019/06/14/8/1560517294111013742.gif",
-    "https://cs11.pikabu.ru/post_img/2019/06/14/8/1560517238115787100.gif",
-    "https://cs7.pikabu.ru/post_img/2019/06/14/8/156051726414098019.gif",
-    "https://cs7.pikabu.ru/post_img/2019/06/14/8/15605172431238525.gif",
-    "https://cs10.pikabu.ru/post_img/2019/06/14/8/1560517177190448543.gif",
-    "https://cs13.pikabu.ru/post_img/2019/06/14/8/1560517198188894105.gif",
-    "https://cs11.pikabu.ru/post_img/2019/06/14/8/1560517203152341690.gif",
-    "https://cs10.pikabu.ru/post_img/2019/06/14/8/1560517207120834751.gif",
-    "https://cs7.pikabu.ru/post_img/2019/06/14/8/1560517210125498145.gif",
-    "https://cs11.pikabu.ru/post_img/2019/06/14/8/1560517218171725970.gif",
-];
-
 const SECS_TO_BAN: u64 = 1800; // 30 min
 
-pub struct SaintnosubBot<'a> {
-    api: Api,
-    hot_listing: &'a mut rawr::structures::listing::Listing<'a>,
+pub struct SaintnosubBot {
+    bot: Bot,
     memeless_users: HashSet<User>,
     sender: Sender<User>,
     receiver: Receiver<User>,
+    meme_reader: memes::MemeReader,
 }
 
-impl<'a> SaintnosubBot<'a> {
-    pub fn new(api: Api, hot_listing: &'a mut rawr::structures::listing::Listing<'a>) -> Self {
-        let future = api.send(GetMe);
-        let me = block_on(future).unwrap();
-        let (sender, receiver) = watch::channel(me);
+impl SaintnosubBot {
+    pub fn new(bot: Bot) -> Self {
+        let (sender, receiver) = watch::channel(User::new(0, true, "Foo"));
+
         return SaintnosubBot {
-            api: api,
-            hot_listing: hot_listing,
+            bot: bot,
             memeless_users: HashSet::new(),
             sender: sender,
             receiver: receiver,
+            meme_reader: memes::MemeReader::new(),
         };
     }
-    async fn reply_with_photo(&self, message: Message, link: String) -> Result<(), Error> {
-        let mut photo =
-            telegram_bot::requests::SendPhoto::new(message.chat, InputFileRef::new(link));
-        photo.reply_to(message.id);
-        self.api.send(photo).await?;
+    async fn reply_with_photo(&self, message: Message, link: String) -> ResponseResult<()> {
+        let photo = InputFile::Url(link);
+        self.bot.send_photo(message.chat.id, photo);
         Ok(())
     }
 
-    async fn send_animation(&self, message: Message, link: String) -> Result<(), Error> {
-        let file = InputFileRef::new(link);
-        self.api.send(message.chat.video(file)).await?;
+    async fn send_animation(&self, message: Message, link: String) -> ResponseResult<()> {
+        let file = InputFile::Url(link);
+        self.bot.send_animation(message.chat.id, file);
         Ok(())
     }
 
-    async fn drugs_message(&self, message: Message) -> Result<(), Error> {
-        if let Some(reply_box) = message.reply_to_message {
-            let value = *reply_box;
-            if let MessageOrChannelPost::Message(org_message) = value {
-                self.reply_with_photo(org_message, String::from(ROAD_ROLLER_PIC_LINK))
-                    .await?;
-            }
-        } else {
-            self.reply_with_photo(message, String::from(DOESNT_MATTER_PIC_LINK))
-                .await?;
-        }
+    async fn drugs_message(&self, message: Message) -> ResponseResult<()> {
+        // if let Some(reply_box) = message.reply_to_message {
+        //     let value = *reply_box;
+        //     if let MessageOrChannelPost::Message(org_message) = value {
+        //         self.reply_with_photo(org_message, String::from(ROAD_ROLLER_PIC_LINK))
+        //             .await?;
+        //     }
+        // } else {
+        //     self.reply_with_photo(message, String::from(DOESNT_MATTER_PIC_LINK))
+        //         .await?;
+        // }
+        let photo = InputFile::Url(String::from(ROAD_ROLLER_PIC_LINK));
+        self.bot.send_photo(message.chat.id, photo);
         Ok(())
     }
 
-    async fn wait_for_meme(
-        &mut self,
-        chat: MessageChat,
-        users: HashSet<User>,
-    ) -> Result<(), Error> {
+    async fn wait_for_meme(&mut self, chat: Chat, users: HashSet<User>) -> ResponseResult<()> {
         for user in &users {
             println!("Wait for meme {} ", user.first_name);
         }
         for user in users {
-            let api = self.api.clone();
+            let bot = self.bot.clone();
             let receiver = self.receiver.clone();
-            let chat_copy = chat.clone();
+            let chat_id = chat.id;
             let _future = std::thread::spawn(move || {
                 let start = time::Instant::now();
                 let mut ban = true;
                 let mut duration = start.elapsed();
                 while duration < time::Duration::from_secs(SECS_TO_BAN) {
-                    let lastest_user = receiver.borrow();
-                    if lastest_user.id == user.id {
+                    let latest_user = receiver.borrow();
+                    if latest_user.id == user.id {
                         ban = false;
                         break;
                     }
@@ -114,132 +91,153 @@ impl<'a> SaintnosubBot<'a> {
                 if ban {
                     // 15 mins to send a meme
                     println!("Kick {} ", user.first_name);
-                    let future = api.send(chat_copy.kick(user));
-                    let _result = block_on(future).unwrap();
+                    bot.kick_chat_member(chat_id, user.id);
                 }
             });
         }
         Ok(())
     }
 
-    async fn greet_users(&mut self, message: Message) -> Result<(), Error> {
-        if let MessageKind::NewChatMembers { ref data, .. } = message.kind {
-            let me = self.api.send(GetMe).await?;
-            let mut usernames = String::new();
-            let mut users = HashSet::new();
-            for user in data {
-                if user.id != me.id {
-                    users.insert(user.clone());
-                    self.memeless_users.insert(user.clone());
-                    if let Some(login) = &user.username {
-                        usernames = format!("{} @{}, ", usernames, login);
-                    } else {
-                        usernames = format!("{} {}, ", usernames, user.first_name);
+    async fn greet_users(&mut self, message: Message) -> ResponseResult<()> {
+        let chat_clone = message.chat.clone();
+        match message.kind {
+            MessageKind::NewChatMembers(new_chat_members_struct) => {
+                let mut usernames = String::new();
+                let mut users = HashSet::new();
+                for user in new_chat_members_struct.new_chat_members {
+                    if !user.is_bot {
+                        users.insert(user.clone());
+                        self.memeless_users.insert(user.clone());
+                        if let Some(login) = &user.username {
+                            usernames = format!("{} @{}, ", usernames, login);
+                        } else {
+                            usernames = format!("{} {}, ", usernames, user.first_name);
+                        }
                     }
                 }
+                let greeting = format!("{} мем или бан!", usernames);
+                self.bot.send_message(message.chat.id, greeting);
+                self.wait_for_meme(chat_clone, users).await?;
             }
-            let greeting = format!("{} мем или бан!", usernames);
-            self.api.send(message.clone().text_reply(greeting)).await?;
-            self.wait_for_meme(message.clone().chat, users).await?;
-        }
-        Ok(())
-    }
-    async fn goodbye_user(&self, message: Message) -> Result<(), Error> {
-        if let MessageKind::LeftChatMember { ref data, .. } = message.kind {
-            let result = self.api.send(GetMe).await?;
-            if data.id != result.id {
-                let greeting = String::from("Скатерью дорожка");
-                self.api.send(message.text_reply(greeting)).await?;
-            }
-        }
-        Ok(())
-    }
-
-    async fn send_meme(&mut self, message: Message) -> Result<(), Error> {
-        let reply = self.get_last_meme();
-        let mut text = String::from(WTF_PIC_LINK);
-        if let Some(title) = reply {
-            text = title;
-        }
-        self.reply_with_photo(message, text).await?;
-        Ok(())
-    }
-    async fn parse_message(&self, message: Message) -> Result<(), Error> {
-        if let MessageKind::Text { ref data, .. } = message.kind {
-            let text = data.as_str().to_lowercase();
-            if text.contains("дементий") {
-                let random_pig_link = *PIGS_LINKS.choose(&mut rand::thread_rng()).unwrap();
-                self.send_animation(message, String::from(random_pig_link.clone()))
-                    .await?;
-            }
-        }
-        Ok(())
-    }
-    async fn parse_photo(&mut self, message: Message) -> Result<(), Error> {
-        if let MessageKind::Photo { .. } = message.kind {
-            println!("There is photo from {}", message.from.first_name);
-            if !message.from.is_bot {
-                if self.memeless_users.contains(&message.from) {
-                    println!("Removed {} fom possible bans", message.from.first_name);
-                    self.memeless_users.remove(&message.from);
-                    let _result = self.sender.broadcast(message.from);
-                }
-            }
-        }
-        Ok(())
-    }
-
-    async fn handle_message(&mut self, message: Message) -> Result<(), Error> {
-        match message.kind {
-            MessageKind::Text { ref data, .. } => match data.as_str() {
-                // "/start" => start_message(api, message).await?,
-                "/tabletki" => self.drugs_message(message).await?,
-                "/jojomeme" => self.send_meme(message).await?,
-                "/jojomeme@saintnosubbot" => self.send_meme(message).await?,
-                _ => self.parse_message(message).await?,
-            },
-            MessageKind::NewChatMembers { .. } => self.greet_users(message).await?,
-            MessageKind::LeftChatMember { .. } => self.goodbye_user(message).await?,
-            MessageKind::Photo { .. } => self.parse_photo(message).await?,
             _ => (),
-        };
+        }
+
         Ok(())
     }
-    fn get_last_meme(&mut self) -> Option<String> {
-        if let Some(post) = self.hot_listing.next() {
-            return post.link_url();
+    async fn goodbye_user(&self, message: Message) -> ResponseResult<()> {
+        if let MessageKind::LeftChatMember(..) = message.kind {
+            let greeting = String::from("Скатерью дорожка");
+            self.bot.send_message(message.chat.id, greeting);
         }
-        return None;
+        Ok(())
     }
+
+    async fn send_meme(&mut self, message: Message) -> ResponseResult<()> {
+        let link = self.meme_reader.get_meme();
+        self.reply_with_photo(message, link).await?;
+        Ok(())
+    }
+    async fn parse_message(&self, message: Message) -> ResponseResult<()> {
+        let message_clone = message.clone();
+        match message.kind {
+            MessageKind::Common(common_message) => match common_message.media_kind {
+                MediaKind::Text(media_text) => {
+                    let text = media_text.text.to_lowercase();
+                    if text.contains("дементий") {
+                        self.send_animation(message_clone, memes::get_random_pig())
+                            .await?;
+                    }
+                }
+                _ => (),
+            },
+            _ => (),
+        }
+        Ok(())
+    }
+    async fn parse_photo(&mut self, message: Message) -> ResponseResult<()> {
+        let message_clone = message.clone();
+        match message.kind {
+            MessageKind::Common(common_message) => match common_message.media_kind {
+                MediaKind::Photo(..) => {
+                    if self.memeless_users.contains(&message_clone.from().unwrap()) {
+                        log::info!(
+                            "Removed {} fom possible bans",
+                            message_clone.from().unwrap().first_name
+                        );
+                        self.memeless_users.remove(&message_clone.from().unwrap());
+                        let _result = self.sender.broadcast(message_clone.from().unwrap().clone());
+                    }
+                }
+                _ => (),
+            },
+            _ => (),
+        }
+        Ok(())
+    }
+
+    async fn handle_message(&mut self, message: Message) -> ResponseResult<()> {
+        let message_clone = message.clone();
+        match message.kind {
+            MessageKind::Common(common_message) => match common_message.media_kind {
+                MediaKind::Text(media_text) => match media_text.text.as_str() {
+                    "/tabletki" => self.drugs_message(message_clone).await?,
+                    "/jojomeme" => self.send_meme(message_clone).await?,
+                    "/jojomeme@saintnosubbot" => self.send_meme(message_clone).await?,
+                    _ => self.parse_message(message_clone).await?,
+                },
+                MediaKind::Photo(..) => self.parse_photo(message_clone).await?,
+                _ => (),
+            },
+            MessageKind::NewChatMembers(..) => self.greet_users(message_clone).await?,
+            MessageKind::LeftChatMember(..) => self.goodbye_user(message_clone).await?,
+            _ => (),
+        }
+        Ok(())
+    }
+
+    // async fn run(&mut self) -> ResponseResult<()> {
+    //     teloxide::repl(self.bot, |message| async move {
+    //         return self.handle_message(message.update).await;
+    //     })
+    //     .await;
+    // }
+}
+
+// Kick a user with a replied message.
+async fn kick_user(cx: &UpdateWithCx<Message>) -> ResponseResult<()> {
+    match cx.update.reply_to_message() {
+        Some(mes) => {
+            // bot.unban_chat_member can also kicks a user from a group chat.
+            cx.bot
+                .unban_chat_member(cx.update.chat_id(), mes.from().unwrap().id)
+                .send()
+                .await?;
+        }
+        None => {
+            cx.reply_to("Use this command in reply to another message")
+                .send()
+                .await?;
+        }
+    }
+    Ok(())
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
-    let token = env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
+async fn main() {
+    teloxide::enable_logging!();
+    log::info!("Starting saintnosub_bot...");
 
-    let api = Api::new(token);
-    let mut stream = api.stream();
+    let bot = Bot::from_env();
 
-    let client = RedditClient::new(
-        "linux:saintnosubbot:v0.0.1 (by /u/Lemiort)",
-        AnonymousAuthenticator::new(),
-    );
-    // Access the subreddit /r/ShitPostCrusaders.
-    let subreddit = client.subreddit("ShitPostCrusaders");
+    let mut saintnosub_bot = SaintnosubBot::new(bot.clone());
 
-    // Gets the hot listing of /r/ShitPostCrusaders. If the API request fails, we will panic with `expect`.
-    let mut hot_listing = subreddit
-        .new(ListingOptions::default())
-        .expect("Could not fetch post listing!");
+    let bot_box = Box::new(&mut saintnosub_bot);
 
-    let mut bot = SaintnosubBot::new(api.clone(), &mut hot_listing);
+    let handler = |message: UpdateWithCx<Message>| async {
+        let unboxed_bot = *bot_box;
+        *bot_box.handle_message(message.update).await?;
+        return ResponseResult::<()>::Ok(());
+    };
 
-    while let Some(update) = stream.next().await {
-        let update = update?;
-        if let UpdateKind::Message(message) = update.kind {
-            bot.handle_message(message).await?;
-        }
-    }
-
-    Ok(())
+    teloxide::repl(bot, handler).await;
 }
---
+++ src/memes.rs
@@ -0,0 +1,77 @@
+use futures::executor::block_on;
+use rand::seq::SliceRandom;
+use roux::util::FeedOption;
+use roux::Subreddit;
+
+const MEMES_COUNT: u32 = 25;
+
+pub struct MemeReader {
+    subreddit: roux::subreddit::Subreddit,
+    listing: roux::subreddit::responses::submissions::Submissions,
+    memes_buffer: Vec<String>,
+    memes_iter: usize,
+}
+
+impl MemeReader {
+    pub fn new() -> Self {
+        let subreddit = Subreddit::new("ShitpostCrusaders");
+        let hot = block_on(subreddit.rising(MEMES_COUNT, None)).unwrap();
+        let iter = hot.data.children.iter();
+        let mut memes_buffer = Vec::new();
+        for value in iter {
+            let link = value.data.url.as_ref().unwrap().clone();
+            memes_buffer.push(link);
+        }
+        return MemeReader {
+            subreddit: subreddit,
+            listing: hot,
+            memes_buffer: memes_buffer,
+            memes_iter: 0,
+        };
+    }
+
+    pub fn get_meme(&mut self) -> String {
+        let result = self.memes_buffer[self.memes_iter].clone();
+        self.memes_iter += 1;
+        if self.memes_iter == self.memes_buffer.len() {
+            // get memes after current listing
+            let after = self.listing.data.after.as_ref().unwrap().clone();
+            let options = FeedOption::new().after(&after);
+            let hot = block_on(self.subreddit.hot(MEMES_COUNT, Some(options))).unwrap();
+            // extract memes to buffer
+            let iter = hot.data.children.iter();
+            let mut memes_buffer = Vec::new();
+            for value in iter {
+                let link = value.data.url.as_ref().unwrap().clone();
+                memes_buffer.push(link);
+            }
+            // override current object
+            self.listing = hot;
+            self.memes_buffer = memes_buffer;
+            self.memes_iter = 0;
+        }
+        // filter for images
+        if result.ends_with(".jpg") || result.ends_with(".png") {
+            return result;
+        } else {
+            return self.get_meme();
+        }
+    }
+}
+
+pub fn get_random_pig() -> String {
+    const PIGS_LINKS: &'static [&'static str] = &[
+        "https://cs10.pikabu.ru/post_img/2019/06/14/8/1560517294111013742.gif",
+        "https://cs11.pikabu.ru/post_img/2019/06/14/8/1560517238115787100.gif",
+        "https://cs7.pikabu.ru/post_img/2019/06/14/8/156051726414098019.gif",
+        "https://cs7.pikabu.ru/post_img/2019/06/14/8/15605172431238525.gif",
+        "https://cs10.pikabu.ru/post_img/2019/06/14/8/1560517177190448543.gif",
+        "https://cs13.pikabu.ru/post_img/2019/06/14/8/1560517198188894105.gif",
+        "https://cs11.pikabu.ru/post_img/2019/06/14/8/1560517203152341690.gif",
+        "https://cs10.pikabu.ru/post_img/2019/06/14/8/1560517207120834751.gif",
+        "https://cs7.pikabu.ru/post_img/2019/06/14/8/1560517210125498145.gif",
+        "https://cs11.pikabu.ru/post_img/2019/06/14/8/1560517218171725970.gif",
+    ];
+    let random_pig_link = *PIGS_LINKS.choose(&mut rand::thread_rng()).unwrap();
+    return String::from(random_pig_link.clone());
+}
